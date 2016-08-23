52ac2ce19a50109ebd3dfc93c72f2d97238d461a
Ed Bond <ed.bond@mere-tech.com>
2016-08-22 23:05:14 -0500

    new schemes for tvos and osx to have it work with carthage

---
+++ SwiftDDP-tvOS/SwiftDDP-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDDP-tvOS.h
+//  SwiftDDP-tvOS
+//
+//  Created by ebond on 8/22/16.
+//  Copyright © 2016 Peter Siegesmund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDDP-tvOS.
+FOUNDATION_EXPORT double SwiftDDP_tvOSVersionNumber;
+
+//! Project version string for SwiftDDP-tvOS.
+FOUNDATION_EXPORT const unsigned char SwiftDDP_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDDP_tvOS/PublicHeader.h>
+
+
